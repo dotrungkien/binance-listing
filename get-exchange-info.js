39c2a540c8c001dd2a023cb40543c3a059c2e611
Do Trung Kien <trungkien.keio@gmail.com>
2020-09-01 22:04:21 +0700

    implement listing

---
+++ get-exchange-info.js
@@ -0,0 +1,52 @@
+const request = require('request');
+fs = require('fs');
+
+const getLocalMarketData = () => {
+  const data = fs.readFileSync('symbols.json');
+  return JSON.parse(data);
+};
+
+const saveMarketData = symbols => {
+  fs.writeFile('symbols.json', JSON.stringify(symbols), function (err, data) {
+    if (err) {
+      return console.log(err);
+    }
+  });
+};
+
+const getNewData = (currentArr, newArr) => {
+  if (!currentArr || !newArr) return [];
+  let diff = [];
+  newArr.forEach(symbol => {
+    if (!currentArr.includes(symbol)) {
+      diff.push(symbol);
+    }
+  });
+  return diff;
+};
+
+const main = () => {
+  const url = 'https://api.binance.com/api/v3/exchangeInfo';
+  request.get(url, (error, response, body) => {
+    if (response.statusCode === 200) {
+      let symbols = [];
+      JSON.parse(body).symbols.forEach(s => {
+        const symbol = s.symbol;
+        if (symbol.slice(-4) === 'USDT') symbols.push(symbol);
+      });
+
+      const currentSymbols = getLocalMarketData();
+      let newSymbols = getNewData(currentSymbols, symbols);
+      if (newSymbols.length === 0) {
+        console.log('no new symbol');
+      } else {
+        console.log('new symbol found!');
+        console.log(newSymbols.join(' '));
+        //cw here
+        saveMarketData(symbols);
+      }
+    }
+  });
+};
+
+main();
